92d9ab8c3a580ad5423fdf4c70b4c8de0c86e4f9
SHIMANTO50 <shimantotsp@gmail.com>
2022-05-19 06:28:25 +0600

    task add,delete complete

--- index.js
+++ index.js
@@ -9,6 +9,45 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n8nqs.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('todoapp').collection('tasks');
+
+        app.get('/task', async (req, res) => {
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const task = await cursor.toArray();
+            res.send(task);
+        });
+
+        app.post('/task', async (req, res) => {
+            const newTask = req.body;
+            console.log('adding new Task', newTask);
+            const result = await taskCollection.insertOne(newTask);
+            res.send(result);
+
+        });
+
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Server Running');
 });
